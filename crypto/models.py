089e687dcb0cc4cb95b78e4201bc3ac31791eb1f
sayanghosh-28 <119283018+sayanghosh-28@users.noreply.github.com>
2024-06-08 22:01:55 +0530

    Add files via upload

--- crypto/models.py
+++ crypto/models.py
@@ -1,3 +1,12 @@
+# crypto/models.py
+
 from django.db import models
 
-# Create your models here.
+class Coin(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10)
+    market_cap = models.BigIntegerField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
